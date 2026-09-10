d64ac441c0d8c5258bb30534b2c7c63df05dc7b4
kevinuncle <kevinrinkenns+github@gmail.com>
2026-09-10 11:23:11 +0200

    test5
    5

--- MessyCoderChallenge/database.inc.php
+++ MessyCoderChallenge/database.inc.php
@@ -8,14 +8,17 @@
     $password = "root"; //default is leeg of root
     $database = "db_awfulportfolio"; //de naam van de database die je aanmaakt
 
+    // Controleer of de MySQLi-extensie beschikbaar is
+    if (!function_exists('mysqli_connect')) {
+        die("MySQLi PHP-extensie is niet ingeschakeld. Schakel deze in php.ini.");
+    }
+
     // Maak verbinding met de database
     $conn = mysqli_connect($host, $username, $password, $database);
 
     // Controleer de verbinding
-    if (function_exists('mysqli_connect_error') && mysqli_connect_error()) {
+    if (!$conn) {
         die("Verbinding mislukt: " . mysqli_connect_error());
-    } else {
-        echo "Verbinding geslaagd!";
     }
 
 ?>
