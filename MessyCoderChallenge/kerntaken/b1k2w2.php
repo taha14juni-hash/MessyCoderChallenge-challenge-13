30366f4a9cd19ec2019d15c1b09171189c8c4312
Taha <taha14juni@gmail.com>
2026-09-15 12:51:56 +0200

    Update b1k2w2.php

--- MessyCoderChallenge/kerntaken/b1k2w2.php
+++ MessyCoderChallenge/kerntaken/b1k2w2.php
@@ -156,7 +156,7 @@
     <nav>
       <ul>
         <li><a href="about.php">Over Mij</a></li>
-        <li><a href="projet.php">Projecten</a></li>
+        <li><a href="projects.php">Projecten</a></li>
         <li><a href="kerntaken.php">Kerntaken</a></li>
         <li><a href="contact.php">Contact</a></li>
       </ul>
